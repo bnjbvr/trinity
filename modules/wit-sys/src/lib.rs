f7a22e6db5c1c9b142ec54c5adabf637ae6cbe0c
Benjamin Bouvier <public@benj.me>
2022-11-11 19:21:07 +0100

    Rename imports.wit to sys.wit and exports.wit to trinity-module.wit

--- modules/wit-sys/src/lib.rs
+++ modules/wit-sys/src/lib.rs
@@ -1,9 +1,9 @@
 mod wit {
     wit_bindgen_guest_rust::generate!({
-        import: "../../wit/imports.wit",
+        import: "../../wit/sys.wit",
         name: "sys"
     });
-    pub use self::imports::*;
+    pub use self::sys::*;
 }
 
 pub use wit::rand_u64;
