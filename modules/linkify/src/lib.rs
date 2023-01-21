91f03a98305d3828b839af9f37fff4e248f0e0cf
Andrew Halberstadt <ahal@pm.me>
2023-01-20 22:50:29 -0500

    feat: Add a 'linkify' module to allow regex substitution of messages

---
+++ modules/linkify/src/lib.rs
@@ -0,0 +1,251 @@
+use anyhow::Context as _;
+use bindings::interface;
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use shlex;
+use textwrap_macros::dedent;
+
+use wit_log as log;
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+struct Rule {
+    name: String,
+    re: String,
+    sub: String,
+}
+
+impl TryFrom<&str> for Rule {
+    type Error = String;
+
+    fn try_from(cmd: &str) -> anyhow::Result<Self, Self::Error> {
+        let words = shlex::split(&cmd).unwrap_or_default();
+        if words.len() != 3 {
+            return Err(String::from("Three inputs expected!"));
+        }
+
+        Ok(Self {
+            name: words[0].clone(),
+            re: words[1].clone(),
+            sub: words[2].clone(),
+        })
+    }
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+struct RoomConfig {
+    enabled_rules: Vec<String>,
+}
+
+struct Component;
+
+impl Component {
+    fn get_rules() -> Vec<Rule> {
+        if let Ok(rules) = wit_kv::get::<_, Vec<Rule>>("rules") {
+            let rules = rules.unwrap_or_default();
+            return rules;
+        }
+        vec![]
+    }
+
+    fn get_room_config(room: &str) -> RoomConfig {
+        if let Ok(rc) = wit_kv::get::<_, RoomConfig>(&format!("room:{}", room)) {
+            let rc = rc.unwrap_or_default();
+            return rc;
+        }
+        RoomConfig {
+            enabled_rules: vec![],
+        }
+    }
+
+    fn replace(msg: &str, rc: RoomConfig) -> Option<String> {
+        for rule in Self::get_rules()
+            .iter()
+            .filter(|&r| rc.enabled_rules.contains(&r.name))
+        {
+            let re = Regex::new(&rule.re).ok()?;
+            if let Some(caps) = re.captures(msg) {
+                let mut dest = String::new();
+                caps.expand(&rule.sub, &mut dest);
+                return Some(dest);
+            }
+        }
+        None
+    }
+
+    fn handle_admin(cmd: &str, _sender: &str, room: &str) -> anyhow::Result<String> {
+        // Format: new NAME RE SUB
+        if let Some(input) = cmd.strip_prefix("new") {
+            let mut rules = Self::get_rules();
+            let rule = match Rule::try_from(input) {
+                Ok(r) => r,
+                Err(e) => return Ok(format!("Error parsing rule: {}", e)),
+            };
+
+            // Don't overwrite existing rules
+            if let Some(_) = rules.iter().find(|&r| r.name == rule.name) {
+                return Ok(format!("Rule '{}' already exists!", &rule.name));
+            }
+
+            // Ensure the regex is valid
+            if Regex::new(&rule.re).is_err() {
+                return Ok(format!("Invalid regex `{}`!", &rule.re));
+            }
+
+            rules.push(rule);
+            let _ = wit_kv::set("rules", &rules);
+            return Ok("Rule has been created!".into());
+        }
+
+        // Format: delete NAME
+        if let Some(cmd) = cmd.strip_prefix("delete") {
+            let mut split = cmd.trim().split_whitespace();
+            let name = split.next().context("missing name")?;
+            let mut rules = Self::get_rules();
+            if let Some(_) = rules.iter().find(|&r| r.name == name) {
+                rules.retain(|r| r.name != name);
+                let _ = wit_kv::set("rules", &rules);
+                return Ok("Rule has been deleted!".into());
+            }
+            return Ok(format!("Rule '{}' not found!", &name));
+        }
+
+        // Format: enable NAME
+        if let Some(cmd) = cmd.strip_prefix("enable") {
+            let mut split = cmd.trim().split_whitespace();
+            let name = split.next().context("missing name")?;
+            let rules = Self::get_rules();
+            if let Some(_) = rules.iter().find(|&r| r.name == name) {
+                let mut rc = Self::get_room_config(room);
+                if rc.enabled_rules.contains(&name.to_string()) {
+                    return Ok(format!("Rule '{}' is already enabled!", &name));
+                }
+                rc.enabled_rules.push(name.to_string());
+                let _ = wit_kv::set(&format!("room:{}", &room), &rc);
+                return Ok(format!("Rule '{}' has been enabled!", &name));
+            }
+            return Ok(format!("Rule '{}' not found!", &name));
+        }
+
+        // Format: disable NAME
+        if let Some(cmd) = cmd.strip_prefix("disable") {
+            let mut split = cmd.trim().split_whitespace();
+            let name = split.next().context("missing name")?;
+            let rules = Self::get_rules();
+            if let Some(_) = rules.iter().find(|&r| r.name == name) {
+                let mut rc = Self::get_room_config(room);
+                if rc.enabled_rules.contains(&name.to_string()) {
+                    rc.enabled_rules.retain(|r| r != &name);
+                    let _ = wit_kv::set(&format!("room:{}", &room), &rc);
+                    return Ok(format!("Rule '{}' has been disabled!", &name));
+                }
+                return Ok(format!("Rule '{}' is already disabled!", &name));
+            }
+            return Ok(format!("Rule '{}' not found!", &name));
+        }
+
+        // Format: list
+        if cmd == "list" {
+            let mut msg = String::new();
+            for rule in Self::get_rules() {
+                msg.push_str(&format!("\n{:?}", &rule));
+            }
+            return Ok(msg);
+        }
+
+        Ok(format!(
+            "Unknown command '{}'!",
+            cmd.split_whitespace().next().unwrap_or("none")
+        ))
+    }
+}
+
+impl interface::Interface for Component {
+    fn init() {
+        let _ = log::set_boxed_logger(Box::new(log::WitLog::new()));
+        log::set_max_level(log::LevelFilter::Trace);
+        log::trace!("Called the init() method \\o/");
+    }
+
+    fn help(topic: Option<String>) -> String {
+        if let Some(topic) = topic {
+            match topic.as_str() {
+                "admin" => dedent!(
+                    r#"
+                    ### Command Overview
+
+                    Available admin commands:
+
+                    - new #NAME #RE #SUB
+                    - delete #NAME
+                    - enable #NAME
+                    - disable #NAME
+                    - list
+
+                    ### Creating and Enabling Rules
+
+                    Rules must first be created using the `new` command, then enabled on a
+                    room by room basis. To create a rule, run
+                    `!admin linkify new <name> <regex> <substitution>`. Where `<name>` is an
+                    identifier to refer to the rule for future use, `<regex>` is a regular expression
+                    to match on text, and `<substitution>` is a string into which the regex capture
+                    groups can be interpolated.
+
+                    For example, to create a new rule which links to a Github issue, run something
+                    like:
+
+                        !admin linkify new issue "(issue ?#?|# ?)([0-9]+)(\\s|$)" https://github.com/bnjbvr/trinity/issues/$2
+
+                    The `$2` will be substituted with the second regex capture group (which is the
+                    issue number in this example). It's also possible to use named capture groups, e.g:
+
+                        !admin linkify new issue "(issue ?#?|# ?)(?P<issue>[0-9]+)(\\s|$)" https://github.com/bnjbvr/trinity/issues/${issue}
+
+                    Then for each room you'd like this rule enabled, run:
+
+                        !admin linkify enable issue
+
+                    Now anytime someone types a string like issue 123 or #123, linkify will respond
+                    with the appropriate URL.
+
+                    The `disable` and `delete` commands take a single `<name>` argument and will
+                    disable the rule, or delete it globally respectively.
+
+            "#
+                )
+                .into(),
+                _ => "Invalid command!".into(),
+            }
+        } else {
+            "Create regex based substition rules per channel! Help topics: admin".to_owned()
+        }
+    }
+
+    fn on_msg(
+        content: String,
+        author_id: String,
+        _author_name: String,
+        room: String,
+    ) -> Vec<interface::Message> {
+        if let Some(content) = Self::replace(&content, Self::get_room_config(&room)) {
+            vec![interface::Message {
+                content,
+                to: author_id,
+            }]
+        } else {
+            vec![]
+        }
+    }
+
+    fn admin(cmd: String, author: String, room: String) -> Vec<interface::Message> {
+        let content = match Self::handle_admin(&cmd, &author, &room) {
+            Ok(resp) => resp,
+            Err(err) => err.to_string(),
+        };
+
+        vec![interface::Message {
+            content,
+            to: author,
+        }]
+    }
+}
+bindings::export!(Component);
