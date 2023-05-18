4fcbf7a740d2b0b23f41e71477234d8000647d05
Benjamin Bouvier <public@benj.me>
2023-05-18 22:23:24 +0200

    Fix release build with cargo-component, and reenable it in the Docker iamge
    Signed-off-by: Benjamin Bouvier <public@benj.me>

--- modules/wit-log/src/lib.rs
+++ modules/wit-log/src/lib.rs
@@ -1,49 +1,46 @@
-mod wit {
-    wit_bindgen::generate!("log" in "../../wit/log.wit");
-    pub use self::log::*;
-}
+wit_bindgen::generate!("log" in "../../wit/log.wit");
 
-pub use log::*;
+pub use ::log::*;
 
 /// A log implementation based on calls to the host.
 pub struct WitLog {
     enabled: bool,
-    max_level: log::LevelFilter,
+    max_level: ::log::LevelFilter,
 }
 
 impl WitLog {
     pub fn new() -> Self {
         Self {
             enabled: true,
-            max_level: log::LevelFilter::Trace,
+            max_level: ::log::LevelFilter::Trace,
         }
     }
     pub fn set_enabled(&mut self, enabled: bool) {
         self.enabled = enabled;
     }
-    pub fn set_max_level(&mut self, level: log::LevelFilter) {
+    pub fn set_max_level(&mut self, level: ::log::LevelFilter) {
         self.max_level = level;
     }
 }
 
-impl log::Log for WitLog {
-    fn enabled(&self, metadata: &log::Metadata) -> bool {
+impl ::log::Log for WitLog {
+    fn enabled(&self, metadata: &::log::Metadata) -> bool {
         self.enabled && metadata.level().to_level_filter() <= self.max_level
     }
 
-    fn log(&self, record: &log::Record) {
+    fn log(&self, record: &::log::Record) {
         if !self.enabled(record.metadata()) {
             return;
         }
 
         let content = format!("{}", record.args());
         match record.level().to_level_filter() {
-            log::LevelFilter::Off => {}
-            log::LevelFilter::Error => wit::error(&content),
-            log::LevelFilter::Warn => wit::warn(&content),
-            log::LevelFilter::Info => wit::info(&content),
-            log::LevelFilter::Debug => wit::debug(&content),
-            log::LevelFilter::Trace => wit::trace(&content),
+            ::log::LevelFilter::Off => {}
+            ::log::LevelFilter::Error => log::error(&content),
+            ::log::LevelFilter::Warn => log::warn(&content),
+            ::log::LevelFilter::Info => log::info(&content),
+            ::log::LevelFilter::Debug => log::debug(&content),
+            ::log::LevelFilter::Trace => log::trace(&content),
         }
     }
 
