3a6f39688566afadc6521904c68f4e2b0e730348
Benjamin Bouvier <public@benj.me>
2023-06-03 13:57:08 +0200

    Add `--locked` when installing cargo-component, so the pinned version is used by default
    Signed-off-by: Benjamin Bouvier <public@benj.me>

--- modules/install-cargo-component.sh
+++ modules/install-cargo-component.sh
@@ -1,2 +1,2 @@
 #/bin/env sh
-cargo install --git https://github.com/bytecodealliance/cargo-component --rev 58b177bec15247b6f4d3a698c407e8c9266d9bec
+cargo install --locked --git https://github.com/bytecodealliance/cargo-component --rev 58b177bec15247b6f4d3a698c407e8c9266d9bec
