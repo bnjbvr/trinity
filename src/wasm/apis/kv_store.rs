772f53e4d3f8019f93c07c1ac5e0866ae955cffa
Benjamin Bouvier <benjamin@bouvier.cc>
2025-01-23 22:46:54 +0100

    refactor(kv): split impl from trait impl
    This avoids the inner closure, which is a bit of a hacky pattern.

--- src/wasm/apis/kv_store.rs
+++ src/wasm/apis/kv_store.rs
@@ -27,59 +27,64 @@ impl KeyValueStoreApi {
     ) -> anyhow::Result<()> {
         kv::add_to_linker(linker, move |s| &mut s.imports[id].apis.kv_store)
     }
+
+    fn set_impl(&mut self, key: Vec<u8>, value: Vec<u8>) -> anyhow::Result<()> {
+        let table_def = TableDefinition::<[u8], [u8]>::new(&self.module_name);
+        let txn = self.db.begin_write()?;
+        {
+            let mut table = txn.open_table(table_def)?;
+            table.insert(&key, &value)?;
+        }
+        txn.commit()?;
+        Ok(())
+    }
+
+    fn get_impl(&mut self, key: Vec<u8>) -> anyhow::Result<Option<Vec<u8>>> {
+        let table_def = TableDefinition::<[u8], [u8]>::new(&self.module_name);
+        let txn = self.db.begin_read()?;
+        let table = match txn.open_table(table_def) {
+            Ok(table) => table,
+            Err(err) => match err {
+                redb::Error::DatabaseAlreadyOpen
+                | redb::Error::InvalidSavepoint
+                | redb::Error::Corrupted(_)
+                | redb::Error::TableTypeMismatch(_)
+                | redb::Error::DbSizeMismatch { .. }
+                | redb::Error::TableAlreadyOpen(_, _)
+                | redb::Error::OutOfSpace
+                | redb::Error::Io(_)
+                | redb::Error::LockPoisoned(_) => Err(err)?,
+                redb::Error::TableDoesNotExist(_) => return Ok(None),
+            },
+        };
+        Ok(table.get(&key)?.map(|val| val.to_vec()))
+    }
+
+    fn remove_impl(&mut self, key: Vec<u8>) -> anyhow::Result<()> {
+        let table_def = TableDefinition::<[u8], [u8]>::new(&self.module_name);
+        let txn = self.db.begin_write()?;
+        {
+            let mut table = txn.open_table(table_def)?;
+            table.remove(&key)?;
+        }
+        txn.commit()?;
+        Ok(())
+    }
 }
 
 impl kv::Host for KeyValueStoreApi {
     fn set(&mut self, key: Vec<u8>, value: Vec<u8>) -> Result<(), kv::KvError> {
-        let closure = || {
-            let table_def = TableDefinition::<[u8], [u8]>::new(&self.module_name);
-            let txn = self.db.begin_write()?;
-            {
-                let mut table = txn.open_table(table_def)?;
-                table.insert(&key, &value)?;
-            }
-            txn.commit()?;
-            Ok(())
-        };
-
-        closure().map_err(|err: anyhow::Error| kv::KvError::Internal(err.to_string()))
+        self.set_impl(key, value)
+            .map_err(|err: anyhow::Error| kv::KvError::Internal(err.to_string()))
     }
 
     fn get(&mut self, key: Vec<u8>) -> Result<Option<Vec<u8>>, kv::KvError> {
-        let closure = || {
-            let table_def = TableDefinition::<[u8], [u8]>::new(&self.module_name);
-            let txn = self.db.begin_read()?;
-            let table = match txn.open_table(table_def) {
-                Ok(table) => table,
-                Err(err) => match err {
-                    redb::Error::DatabaseAlreadyOpen
-                    | redb::Error::InvalidSavepoint
-                    | redb::Error::Corrupted(_)
-                    | redb::Error::TableTypeMismatch(_)
-                    | redb::Error::DbSizeMismatch { .. }
-                    | redb::Error::TableAlreadyOpen(_, _)
-                    | redb::Error::OutOfSpace
-                    | redb::Error::Io(_)
-                    | redb::Error::LockPoisoned(_) => Err(err)?,
-                    redb::Error::TableDoesNotExist(_) => return Ok(None),
-                },
-            };
-            Ok(table.get(&key)?.map(|val| val.to_vec()))
-        };
-        closure().map_err(|err: anyhow::Error| kv::KvError::Internal(err.to_string()))
+        self.get_impl(key)
+            .map_err(|err: anyhow::Error| kv::KvError::Internal(err.to_string()))
     }
 
     fn remove(&mut self, key: Vec<u8>) -> Result<(), kv::KvError> {
-        let closure = || {
-            let table_def = TableDefinition::<[u8], [u8]>::new(&self.module_name);
-            let txn = self.db.begin_write()?;
-            {
-                let mut table = txn.open_table(table_def)?;
-                table.remove(&key)?;
-            }
-            txn.commit()?;
-            Ok(())
-        };
-        closure().map_err(|err: anyhow::Error| kv::KvError::Internal(err.to_string()))
+        self.remove_impl(key)
+            .map_err(|err: anyhow::Error| kv::KvError::Internal(err.to_string()))
     }
 }
